39e873e6c1919e1e058ec0fa3b8ffc76ac4fbabb
Melisa Doko <mdoko@helius-systems.com>
2023-09-03 16:56:44 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import Logo from "./components/Logo";
+import PackingList from "./components/PackingList";
+import Form from "./components/Form";
+import Stats from "./components/Stats";
+import { useState } from "react";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onClearList={handleClearList}
+        onToogleItem={handleToggleItem}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+export default App;
